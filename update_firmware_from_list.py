e0f9095ec738ebbefb54bf82a60057d53efc3fb1
cubicroot <mine_j@idein.jp>
2022-07-14 16:51:22 +0900

    ログ出力機能追加

--- update_firmware_from_list.py
+++ update_firmware_from_list.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
 from os import path
 import sys
 import time
 import datetime
+import logging
 from actcast_api import ActcastAPI, Color
+from logging import StreamHandler, FileHandler, Formatter
+from logging import INFO, DEBUG, NOTSET
 
 request_interval_msec = 500
 
@@ -24,8 +28,8 @@ def update_firmware(api, group_id, id_list_name):
 
   update_fw(api, group_id, id_list_name)
 
-  print('=' * 80)
-  print(f'Done. {datetime.datetime.now()}(JST)')
+  logging.info('=' * 80)
+  logging.info(f'Done. {datetime.datetime.now()}(JST)')
 
 def init_interval_settings(api):
   # SBプロジェクト対応。一定時間内に更新するデバイス数を制限する
@@ -35,7 +39,6 @@ def init_interval_settings(api):
     Setting.devices_per_interval = api.setting_json['update_firmware_devices_per_interval']
 
 def update_fw(api, group_id, id_list_name):
-
   with open(id_list_name) as f:
     while True:
       current_device_id = f.readline()
@@ -63,29 +66,47 @@ def update_single(api, group_id, device_id):
   item = api.firmware_update(group_id, device_id)
 
   if item is False:
-    print(Color.RED+f'└> ERROR updating Firmware for device: {device_id}')
-    print('-' * 80, Color.COLOR_DEFAULT)
+    logging.error(Color.RED+f'└> ERROR updating Firmware for device: {device_id}')
+    logging.error('-' * 80, Color.COLOR_DEFAULT)
   else:
-    print(f'{device_id}')
+    logging.info(f'{device_id}')
 
 def print_latest_fw_info(api, group_id):
   latest_info = api.get_firmware_info(group_id).items[0]
   release_date = api.iso8601toJST(latest_info.release_date)
-  print('Latest firmware')
-  print(f'firmware_type    : {latest_info.firmware_type}')
-  print(f'firmware_version : {latest_info.firmware_version}')
-  print(f'release_date     : {release_date}(JST)')
-  print('=' * 50)
-  print(f'Start. {datetime.datetime.now()}(JST)')
-
+  logging.info('Latest firmware')
+  logging.info(f'firmware_type    : {latest_info.firmware_type}')
+  logging.info(f'firmware_version : {latest_info.firmware_version}')
+  logging.info(f'release_date     : {release_date}(JST)')
+  logging.info('=' * 50)
+  logging.info(f'Start. {datetime.datetime.now()}(JST)')
 
 if __name__ == '__main__':
+    # ログ出力用ストリームハンドラの設定
+  stream_handler = StreamHandler()
+  stream_handler.setLevel(INFO)
+  stream_handler.setFormatter(Formatter("%(message)s"))
+
+  # ログファイル保存先の有無チェック
+  if not os.path.isdir('./Log'):
+      os.makedirs('./Log', exist_ok=True)
+
+  # ファイルハンドラの設定
+  file_handler = FileHandler(
+      f"./Log/update_firmware_from_list_{datetime.datetime.now():%Y%m%d%H%M%S}.log"
+  )
+  file_handler.setLevel(DEBUG)
+  file_handler.setFormatter(
+      Formatter("%(asctime)s@ %(name)s [%(levelname)s] %(funcName)s: %(message)s")
+  ) 
+  logging.basicConfig(level=NOTSET, handlers=[stream_handler, file_handler])
+
   api = ActcastAPI()
 
   args = sys.argv
   if len(args) < 2:
-    print("usage:")
-    print(f"$ python3 {path.basename(__file__)} group_id id_list.txt")
+    logging.info("usage:")
+    logging.info(f"$ python3 {path.basename(__file__)} group_id id_list.txt")
   else:
     group_id  = args[1]
     id_list_name   = args[2]
