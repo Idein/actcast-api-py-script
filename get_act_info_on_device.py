5e8f310f5d91764e135303f1acd2f621fcfd9d13
cubicroot <mine_j@idein.jp>
2022-07-15 18:20:52 +0900

    Act情報取得スクリプト追加

--- get_act_info_on_device.py
+++ get_act_info_on_device.py
@@ -5,7 +5,7 @@ import ast
 import json
 from actcast_api import ActcastAPI
 
-def example(api, group_id, act_id):
+def example(api, group_id, device_id):
 
     # QUERY PARAMETERS
     query_params = {
@@ -15,7 +15,7 @@ def example(api, group_id, act_id):
     }
 
     # Request
-    data = api.get_act_info(group_id, act_id)
+    data = api.get_act_info_on_device(group_id, device_id)
 
     # Print result
     # actcast_api Wrapperの戻り値は正式なJSON形式で返ってこない(シングルクォーテーションが使われている)
@@ -30,6 +30,6 @@ if __name__ == '__main__':
     args = sys.argv
     if len(args) < 3:
         print("usage:")
-        print(f"$ python3 {os.path.basename(__file__)} group_id act_id")
+        print(f"$ python3 {os.path.basename(__file__)} group_id device_id")
     else:
         example(api, args[1], args[2])
